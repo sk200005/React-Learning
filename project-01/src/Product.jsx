3c687838498f7b068c9502046afaff74276e5bc9
sk200005 <swayamkorde2005@gmail.com>
2026-01-28 16:31:38 +0530

    Dynamic Condition Styling

--- project-01/src/Product.jsx
+++ project-01/src/Product.jsx
@@ -1,15 +1,18 @@
 import './Product.css'
 
-function Product( {title , price = "free", features = []}) {
+function Product( {title , price ="free", features = []}) {
     const list = features.map((feat) => <li>{feat}</li>)
+    let isDiscount = price > 30000 ;
+    let styles = {backgroundColor : isDiscount ? "#A45A52" : ""} ;
     return (
       <>
-      <div className='Product'>
+      <div className='Product' style={styles}>
         
         <h2>{title}</h2>
         <h3>Price : {price}</h3>
         <h4>Features : </h4>
         <ul>{list}</ul>
+        {price > 30000 ? <p>"5% Discount" </p> : null}
 
       </div>
       </>
