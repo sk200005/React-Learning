b2c608bc9a035b8e30803674d72dbe969a774ef1
sk200005 <swayamkorde2005@gmail.com>
2026-01-30 19:32:21 +0530

    Add Like toggle state and count version-02

--- project-03-states/src/Like.jsx
+++ project-03-states/src/Like.jsx
@@ -2,19 +2,25 @@ import { useState } from "react";
 
 export default function LikeButton() {
   const [isLiked, setIsLiked] = useState(false);
+  const [clicks, setClicks] = useState(0)
 
   function toggleLike() {
     setIsLiked(prev => !prev);
+    setClicks(clicks + 1)
   }
+  let likeStyle = {color : "red"}
 
   return (
     <div >
 
-      <i className={isLiked ? "fa-solid fa-heart" : "fa-regular fa-heart"} onClick={toggleLike}>
-        
+      <i className={isLiked ? "fa-solid fa-heart": "fa-regular fa-heart"}
+      style={isLiked ? likeStyle : null} onClick={toggleLike}>
       </i>
       <h3> {isLiked ? 1 : 0}</h3>
+      <h5> total :  {clicks}</h5>
+
+
       
-    </div>
+    </div>  
   );
 }
